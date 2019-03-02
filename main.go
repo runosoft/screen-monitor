561dc28894c3a16d3a58317deb1ebf0347539041
hemreari <hemreari98@gmail.com>
2019-03-02 20:26:20 +0300

    add some functions

--- main.go
+++ main.go
@@ -22,7 +22,26 @@ type StorageConfig struct {
 	Name string `json:"name"`
 }
 
+/* Holds screen informations from DB */
+type DBScreens struct {
+	PID string
+	Name string
+}
+
+/* Holds screen informations from command */
+type SystemScreens struct {
+	PID string
+	Name string
+}
+
+/* Holds name of the screen that
+ * we want to check whether active or not */
+type ActiveScreens struct {
+	Names []string `json:"activeScreen"`
+}
+
 var config SQLStruct
+var activeScreens ActiveScreens
 
 func readConfig(cfg *SQLStruct, configFileName string) {
 	configFileName, _ = filepath.Abs(configFileName)
@@ -30,59 +49,122 @@ func readConfig(cfg *SQLStruct, configFileName string) {
 
 	configFile, err := os.Open(configFileName)
 	if err != nil {
-		log.Fatal("File error: ", err.Error())
+		log.Fatalf("Error when opening %s: %s\n", configFilename ,err.Error())
 	}
 	defer configFile.Close()
 	jsonParser := json.NewDecoder(configFile)
 	if err := jsonParser.Decode(&cfg); err != nil {
-		log.Fatal("Config error: ", err.Error())
+		log.Fataf("Config error for %s: %s\n", configFilename, err.Error())
 	}
-
 }
+
+func readActiveScreensConfig(activeScreen *ActiveScreens, configFilename string) {
+	configFileName, _ = filepath.Abs(configFileName)
+	log.Printf("Reading Active Screens: %v", configFileName)
+
+	configFile, err := os.Open(configFileName)
+	if err != nil {
+		log.Fatalf("Error when opening %s: %s\n", configFilename ,err.Error())
+	}
+	defer configFile.Close()
+	jsonParser := json.NewDecoder(configFile)
+	if err := jsonParser.Decode(&activeScreen); err != nil {
+		log.Fataf("Config error for %s: %s\n", configFilename, err.Error())
+	}
+}
+
 func main() {
 	readConfig(&config, "config.json")
+	readActiveScreensConfig(&activeScreens, "active_screen.json")
 
 	db, err := sql.Open(config.Storage.Driver, config.Storage.Name)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	screenStruct := updateScreenList(db)
+	log.Println(screenStruct)
+
+	checkScreens()
+}
+
+func checkScreens(screens []DBScreens) {
+	for i := 0; i < len(screens); i++ {
+		
+	}
+}
+
+/* Parses output of screen -ls command to SystemScreens 
+ * struct. */
+func updateSystemScreen() []SystemScreens{
 	out, err := exec.Command("screen", "-ls").Output()
-
-	//xregexString, _ := regexp.Compile("p([a-z]+)ch")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error while executing screen -ls command: %s\n", err)
 	}
-	//log.Println(out)
 	screenOut := string(out)
-	//screenOut = strings.TrimSpace(screenOut)
-	screenOutArr := strings.Split(screenOut, "\n")
-	screenOut = screenOutArr[1]
-	screenOut = strings.TrimSpace(screenOut)
-	screenOutArr = strings.Split(screenOut, "\t")
-	screenOut = screenOutArr[0]
-	fmt.Println(screenOut)
+	screenOutLineArr := strings.Split(screenOut, "\n")
 
-	screenOutArr = strings.Split(screenOut, ".")
-	screenPID := screenOutArr[0]
-	screenName := screenOutArr[1]
+	var screenStructArr []SystemScreens
+	var pureScreen []string
 
-	fmt.Println("screenPID:" + screenPID)
-	fmt.Println("screenName:" + screenName)
-
-	stmt, err := db.Prepare("INSERT INTO ScreenInfo(PID, screen_name, up) VALUES(?, ?, ?)")
-	if err != nil {
-		log.Println(err)
+	for i := 1; i < len(screenOutLineArr)-2; i++ {
+		pureScreen = append(pureScreen, screenOutLineArr[i])
 	}
 
-	res, err := stmt.Exec(screenPID, screenName, 1)
+	for i := 0; i < len(pureScreen); i++ {
+		screenOut = strings.TrimSpace(pureScreen[i])
+		screenOutArr := strings.Split(screenOut, "\t")
+		screenOut = screenOutArr[0]
+		screenOutArr = strings.Split(screenOut, ".")
+		screenPID := screenOutArr[0]
+		screenName := screenOutArr[1]
+
+		systemScreens := SystemScreens{
+			PID: screenPID,
+			Name: screenName,
+		}
+
+		screenStructArr = append(screenStructArr, SystemScreens)
+	}
+	return screenStructArr
+}
+
+/* Gets screen PIDs, names from DB and parse them to
+ * the DBScreens struct. */
+func updateDBScreen(db *sql.DB) []DBScreens {
+	var screenStructArr []DBScreens
+
+	rows, err := db.Query("SELECT PID, screen_name FROM ScreenInfo")
 	if err != nil {
-		log.Println(err)
+		log.Printf("Error while getting PID and screen_name from DB: %s\n", err)
 	}
 
-	id, err := res.RowsAffected()
-	if err != nil {
-		log.Println(err)
+	var dbScreenPID string
+	var dbScreenName string
+
+	for rows.Next() {
+		err = rows.Scan(&dbScreenPID, &dbScreenName)
+		if err != nil {
+			log.Printf("Error while scanning screen info from DB: %s\n", err)
+		}
+
+		dbScreens := DBScreens{
+			PID: dbScreenPID,
+			Name: dbScreenName,
+		}
+		screenStructArr = append(screenStructArr, screenStruct)
 	}
-	log.Println(id)
+	return screenStructArr
+}
+
+func RowExists(db *sql.DB, pid string) bool {
+	existQuery := "SELECT exists(SELECT PID FROM ScreenInfo WHERE PID=?)"
+	var exists bool
+
+	err := db.QueryRow(existQuery, pid).Scan(&exists)
+	if err != nil && err != sql.ErrNoRows {
+		log.Printf("Error when getting existence of pid from DB: %s\n", err)
+		return false
+	}
+	return exists
 }
